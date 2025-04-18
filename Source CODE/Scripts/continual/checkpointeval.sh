9595e2e1686a8ef15a05fe39f4368cafac8077a4
AnkitindiiitA <ankitsharma.ind.2001@gmail.com>
2025-04-18 19:29:26 +0530

    code fix params

--- Source CODE/Scripts/continual/checkpointeval.sh
+++ Source CODE/Scripts/continual/checkpointeval.sh
@@ -3,4 +3,4 @@ python EvaluateCheckpoint.py --strategy "SynapticIntelligence" --dataset "philad
       --no_epochs 100 --benchmark_configs_path "./BenchmarkConfigs/linear_target-10.yml" \
       --wandb_proj "DeepContinualAuditing" \
       --bottleneck "tanh" --seed $seed --training_regime 'continual' \
-      --checkpoint_path '/outputs/continual_SynapticIntelligence_linear_philadelphia_sNone_25-04-16-17-38-07/ckpt_continual_SynapticIntelligence_linear_philadelphia_sNone_25-04-16-17-38-07_9.pt'
+      --checkpoint_path './outputs/continual_SynapticIntelligence_linear_philadelphia_sNone_25-04-16-17-38-07/ckpt_continual_SynapticIntelligence_linear_philadelphia_sNone_25-04-16-17-38-07_9.pt'
--- Source CODE/UtilsHandler/MetricHandler.py
+++ Source CODE/UtilsHandler/MetricHandler.py
@@ -51,6 +51,10 @@ class MetricHandler(object):
             "rec_losses": recon_losses.detach().cpu().numpy().tolist(),
             "depts": ds[:][3].cpu().numpy().tolist()
         }
+        print("prec")
+        print("tp:", tp)
+        print("fp:", fp)
+        print("end")
 
         return prec, info
 
@@ -74,20 +78,26 @@ class MetricHandler(object):
         recon_losses = torch.cat(recon_losses, dim=0)
 
         adaptive_thresholds = []
-        window_size = 10
+        window_size = 1
         for idx in range(len(ds)):
             start = max(0, idx - window_size)
             end = min(len(ds), idx + window_size)
             local_recon_losses = recon_losses[start:end]
-            adaptive_threshold = 0.9 * torch.mean(local_recon_losses) + 2.8 * torch.std(local_recon_losses)
+            adaptive_threshold = 0.9 * torch.mean(local_recon_losses) + 0.5 * torch.std(local_recon_losses)
             adaptive_thresholds.append(adaptive_threshold)
 
         tp = 0
         fn = 0
+        tp_1000=0
+        tp_2000=0
         for idx in range(len(ds)):
             if ds[idx][3].item() in params["global_anomaly_dept"] or ds[idx][3].item() in params["local_anomaly_dept"]:
                 if recon_losses[idx] > adaptive_thresholds[idx]:
                     tp += 1
+                    if(ds[idx][3].item()==1000):
+                      tp_1000+=1
+                    else: 
+                      tp_2000+=1
                 else:
                     fn += 1
 
@@ -97,5 +107,17 @@ class MetricHandler(object):
             "rec_losses": recon_losses.detach().cpu().numpy().tolist(),
             "depts": ds[:][3].cpu().numpy().tolist()
         }
+        # with open("rec_loss_log.txt", "w") as f:
+        #   for idx in range(len(ds)):
+        #       dept = ds[idx][3].item()
+        #       loss_val = recon_losses[idx].item()
+        #       threshold_val = adaptive_thresholds[idx].item()
+        #       f.write(f"{idx}, dept={dept}, loss={loss_val:.6f}, threshold={threshold_val:.6f}\n")
+        print("rec")
+        print("tp:", tp)
+        print("fp:", fn)
+        print("tp1000:", tp_1000)
+        print("tp2000:", tp_2000)
+        print("end")
 
         return rec, info

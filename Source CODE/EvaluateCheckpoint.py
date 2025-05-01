e7ecfd07d4a8408c4793f8872bf113b5bfcf7a0b
AnkitindiiitA <ankitsharma.ind.2001@gmail.com>
2025-05-01 17:30:42 +0530

    minor changes

--- Source CODE/EvaluateCheckpoint.py
+++ Source CODE/EvaluateCheckpoint.py
@@ -97,6 +97,7 @@ if __name__ == "__main__":
     # synaptic intelligence
     parser.add_argument('--si_lambda', help='', nargs='?', type=float, default=1.00)
     parser.add_argument('--si_eps', help='', nargs='?', type=float, default=0.001)
+    parser.add_argument('--gem_patterns_per_exp', type=int, default=256, help='Number of patterns per experience for GEM')
 
     parser.add_argument('--checkpoint_path', type=str, required=True, help='Path to model checkpoint')
 
